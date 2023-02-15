a4be9b6e819aa37570d1d2e344d76f20524f6852
Casual_P <nk0898@mail.ru>
2023-02-15 21:11:06 +0300

    add cascade persist to roles

--- src/main/java/com/example/springbootproj/entity/User.java
+++ src/main/java/com/example/springbootproj/entity/User.java
@@ -27,7 +27,7 @@ public class User {
     private String password;
     @Column(unique = true, nullable = false)
     private String email;
-    @ManyToMany
+    @ManyToMany(cascade = CascadeType.PERSIST)
     @JoinTable(name = "user_role",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
