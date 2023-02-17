7c30c885ec27e2d89f74b62537d593c00a46c64f
Casual_P <nk0898@mail.ru>
2023-02-17 23:59:57 +0300

    change directory

--- src/main/java/com/example/springbootproj/controller/rest/CommentController.java
+++ src/main/java/com/example/springbootproj/controller/rest/CommentController.java
@@ -1,4 +1,4 @@
-package com.example.springbootproj.controller;
+package com.example.springbootproj.controller.rest;
 
 import com.example.springbootproj.dto.CommentDto;
 import com.example.springbootproj.dto.UserDto;
